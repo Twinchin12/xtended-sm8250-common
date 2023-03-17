5a4a316a1a85cb6227afea0403fe78a25ecdecdb
zahid <zahidhasmani07@gmail.com>
2023-03-17 11:13:10 +0000

    adapt to xtended + neutron clang support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,7 +92,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/evolution/config/device_framework_matrix.xml
+    vendor/xtended/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
@@ -121,7 +121,15 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=oneplus
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
-TARGET_KERNEL_CONFIG := evo_defconfig
+TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
+#TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-neutron
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-neutron/bin
+TARGET_KERNEL_CLANG_VERSION := neutron
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+KERNEL_SUPPORTS_LLVM_TOOLS := true
+#TARGET_CLANG_PREBUILTS_VERSION := neutron
+#TARGET_CLANG_WITH_GNU_BINUTILS := true
+#TARGET_NEEDS_PREBUILT_FLEX_BISON := true
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
